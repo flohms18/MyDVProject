e2ccd3ebd30ea0524bb969c62619162a345adfba
flotr18 <trouilletflorent@outlook.com>
2025-03-10 23:19:04 +0100

    Update settings.py

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4-qs&t&2xyds6iz*&w0*z&z&lqi9v2=vp=1qx^^e1k!64f&@t2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
