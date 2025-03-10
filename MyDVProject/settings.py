e9ae13679c3a4ebe96cc9b4d8d7a9aea4164e8fe
flotr18 <trouilletflorent@outlook.com>
2025-03-11 00:43:45 +0100

    Update settings.py

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/usr/local/lsws/Example/html/dataverse/static'
+STATIC_ROOT = '/usr/local/lsws/Example/html/MyDVProject/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
