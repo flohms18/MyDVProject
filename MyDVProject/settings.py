0ec12cb9e168997dced184932b8a4c32053c9e8e
flotr18 <trouilletflorent@outlook.com>
2025-03-11 01:26:26 +0100

    update

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/usr/local/lsws/Example/html/MyDVProject/static'
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
