930d604f37f6613869b6f7f347e4bd953b405a9a
flotr18 <trouilletflorent@outlook.com>
2025-03-11 21:04:33 +0100

    Final Push

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["dataverse-ft.com","www.dataverse-ft.com"]
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 SECRET_KEY = os.getenv('SECRET_KEY')
