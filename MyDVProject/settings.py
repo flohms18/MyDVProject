460f31f6ac399566c39ba331faab412875d4b5f6
flotr18 <trouilletflorent@outlook.com>
2025-03-11 12:15:11 +0100

    Update settings.py

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -83,10 +83,8 @@ WSGI_APPLICATION = 'MyDVProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
