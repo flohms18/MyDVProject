f34374c0bca12a128aa2ee7953366402758d739c
flotr18 <trouilletflorent@outlook.com>
2025-03-11 17:23:27 +0100

    Update settings.py

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dataverse-ft.com','www.dataverse-ft.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
