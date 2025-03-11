147b6be8c2c60a7318a8906262692f4e1feedefb
flotr18 <trouilletflorent@outlook.com>
2025-03-11 14:43:18 +0100

    Update settings.py

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
+import environ # type: ignore
 
 env = environ.Env(
     # set casting, default value
