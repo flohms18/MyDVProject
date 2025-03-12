452cfab0a3cc903118e2b8fdf62b8da76a74940b
flotr18 <trouilletflorent@outlook.com>
2025-03-12 14:27:49 +0100

    Menu Adjustements

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['dataverse-ft.com','www.dataverse-ft.com']
 DEBUG = os.getenv('DEBUG', 'True') == 'False'
