705e5e46beaa12e823975a4d4b5c69fc36156e84
flohms18 <trouilletflorent@outlook.com>
2025-03-20 15:25:38 +0100

    Update settings.py

--- MyDVProject/settings.py
+++ MyDVProject/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')# SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = ['dataverse-ft.com','www.dataverse-ft.com']
+DEBUG = os.getenv('DEBUG', 'True') == 'False'
 SECRET_KEY = os.getenv('SECRET_KEY')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
@@ -47,6 +48,8 @@ INSTALLED_APPS = [
     'tinymce'
 ]
 
+ROOT_URLCONF = 'MyDVProject.urls'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -59,7 +62,6 @@ MIDDLEWARE = [
     
 ]
 
-ROOT_URLCONF = 'MyDVProject.urls'
 
 TEMPLATES = [
     {
