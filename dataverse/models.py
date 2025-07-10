56e896f7a0a56888d42d796454da6b9b62287a75
flohms18 <trouilletflorent@outlook.com>
2025-07-10 23:36:47 +0200

    New test

--- dataverse/models.py
+++ dataverse/models.py
@@ -45,7 +45,7 @@ class Article(models.Model):
     tool = models.CharField(max_length=255,default="one")
     content = HTMLField()
     is_featured = models.BooleanField(default=False)
-    insight = models.ForeignKey(Insight, on_delete=models.CASCADE, related_name="articles", blank=True,null=True)
+    insight = models.ForeignKey(Insight, on_delete=models.CASCADE, related_name="articles_insight",null=True,blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="articles")
     published_date = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True, blank=True,null=True)  # Add the slug field
