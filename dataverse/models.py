2f5e7a9974caa6d0e734e444b88dc4e8ea925a03
flohms18 <trouilletflorent@outlook.com>
2025-07-11 09:17:54 +0200

    Insight migration

--- dataverse/models.py
+++ dataverse/models.py
@@ -34,13 +34,18 @@ class GlossaryTerm(models.Model):
     def __str__(self):
         return self.term
 
+class Insight(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     tool = models.CharField(max_length=255,default="one")
     content = HTMLField()
     is_featured = models.BooleanField(default=False)
+    insight = models.ForeignKey(Insight, on_delete=models.CASCADE, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="articles")
     published_date = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True, blank=True,null=True)  # Add the slug field
